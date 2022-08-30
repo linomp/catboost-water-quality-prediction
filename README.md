f077afa972000139384dd2b0d57e95892502af93
linomp <lm2p@hotmail.es>
2022-08-30 13:08:43 +0200

    add readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Drinking Water Quality Prediction
+
+The goal of this competition is to create a model that predicts the water quality in Estonian water stations based on
+the government’s open data of the previous measurements.
+
+https://www.kaggle.com/competitions/drinking-water-quality/overview
+
+## Notes
+
+- wittgenstein model interpreting feature has a bug: https://github.com/imoscovitz/wittgenstein/pull/24
--- utils.py
+++ utils.py
@@ -1,3 +1,36 @@
+import wittgenstein as lw
+from catboost import Pool, CatBoostClassifier
+from catboost.utils import get_roc_curve
+from matplotlib import pyplot as plt
+from sklearn.metrics import roc_auc_score
+from wittgenstein.interpret import interpret_model
+
+
+def get_catboost_roc_curve(model, X, y, categorical_features_indexes):
+    # auc = model.best_score_['validation']['AUC']
+    auc = roc_auc_score(y, model.predict_proba(X)[:, 1])
+
+    (fpr, tpr, _) = get_roc_curve(model,
+                                  Pool(X, y, cat_features=categorical_features_indexes, feature_names=list(X.columns)))
+
+    ax = plt.gca()
+    ax.plot(fpr, tpr, color="b",
+            lw=2,
+            alpha=0.8,
+            label="ROC (AUC = %0.2f)" % auc)
+    ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Random classifier", alpha=0.8)
+
+    ax.set(
+        xlim=[-0.05, 1.05],
+        ylim=[-0.05, 1.05],
+    )
+    ax.legend(loc="lower right")
+
+    plt.show()
+
+    return auc
+
+
 def get_ruleset_from_catboost(max_rules, max_rule_conds, max_total_conds, k, dl_allowance, n_discretize_bins,
                               prune_size, train, test, full_dataset, columns_to_ignore, activity, output_dir):
     """
@@ -23,8 +56,7 @@ def get_ruleset_from_catboost(max_rules, max_rule_conds, max_total_conds, k, dl_
               use_best_model=True, eval_set=test_pool)
 
     # Get Catboost model metrics
-    catboost_auc_score = get_catboost_roc_curve_cv(model, X_test, y_test, categorical_features_indexes, activity,
-                                                   output_dir)
+    catboost_auc_score = get_catboost_roc_curve(model, X_test, y_test, categorical_features_indexes)
 
     # interpret with wittgenstein and get a ruleset
     def predict_fn(data, _):
@@ -42,37 +74,3 @@ def get_ruleset_from_catboost(max_rules, max_rule_conds, max_total_conds, k, dl_
     ruleset_params['catboost_total_trees'] = model.tree_count_
 
     return ruleset_model, ruleset_params, model, catboost_feature_importances, catboost_auc_score
-
-
-def get_catboost_roc_curve_cv(model, X, y, categorical_features_indexes, activity, output_dir):
-    try:
-        # auc = model.best_score_['validation']['AUC']
-        auc = roc_auc_score(y, model.predict_proba(X)[:, 1])
-    except:
-        true_count = sum(y)
-        raise Exception(f"{true_count} positive example(s) for \'{activity}\' in test set")
-
-    if not os.getenv("ENABLE_ROC_PLOTS", False):
-        return auc
-
-    (fpr, tpr, _) = get_roc_curve(model,
-                                  Pool(X, y, cat_features=categorical_features_indexes, feature_names=list(X.columns)))
-
-    ax = plt.gca()
-    ax.plot(fpr, tpr, color="b",
-            lw=2,
-            alpha=0.8,
-            label="ROC (AUC = %0.2f)" % auc)
-    ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Random classifier", alpha=0.8)
-
-    ax.set(
-        xlim=[-0.05, 1.05],
-        ylim=[-0.05, 1.05],
-        title=f"Catboost ROC curve for '{activity}'",
-    )
-    ax.legend(loc="lower right")
-
-    plt.savefig(f'{output_dir}/{activity.replace(" ", "_")}_CB_{int(time.time())}.png')
-    plt.show()
-
-    return auc
