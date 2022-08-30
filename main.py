300b4a17fa625000cf62116b4cbeb0cf256a1220
linomp <lm2p@hotmail.es>
2022-08-30 13:51:13 +0200

    base version done, auc~=0.7

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import pandas as pd
 
+from utils import train_catboost_classifier
+from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    target = 'compliance_2021'
+    dataset = pd.read_csv('data/train.csv')
+
+    # # split data into train and test
+    # train = dataset.sample(frac=0.6)
+    # test = dataset.drop(train.index)
+
+    # split stratified with sklearn
+    train, test = train_test_split(dataset, test_size=0.33, stratify=dataset[target])
+
+    # replace all nans with 0
+    train.fillna(0, inplace=True)
+
+    # TODO: explore better splitting & data cleaning strategies
+    # TODO: optimize hyperparams, grid search?
+    # TODO: add cross validation?
+    # TODO: compute and show accuracy
+    # TODO: adapt pipeline to produce output file based on "test" file in expected format
+
+    model = train_catboost_classifier(train, test, target=target)
--- utils.py
+++ utils.py
@@ -1,12 +1,12 @@
-import wittgenstein as lw
-from catboost import Pool, CatBoostClassifier
-from catboost.utils import get_roc_curve
+import shap
 from matplotlib import pyplot as plt
 from sklearn.metrics import roc_auc_score
-from wittgenstein.interpret import interpret_model
+
+from catboost import Pool, CatBoostClassifier
+from catboost.utils import get_roc_curve
 
 
-def get_catboost_roc_curve(model, X, y, categorical_features_indexes):
+def get_catboost_roc_auc(model, X, y, categorical_features_indexes):
     # auc = model.best_score_['validation']['AUC']
     auc = roc_auc_score(y, model.predict_proba(X)[:, 1])
 
@@ -31,46 +31,27 @@ def get_catboost_roc_curve(model, X, y, categorical_features_indexes):
     return auc
 
 
-def get_ruleset_from_catboost(max_rules, max_rule_conds, max_total_conds, k, dl_allowance, n_discretize_bins,
-                              prune_size, train, test, full_dataset, columns_to_ignore, activity, output_dir):
-    """
-    Prototype of interpretable model extraction: CatBoost classifier then Ruleset
-    - https://github.com/imoscovitz/wittgenstein#interpreter-models
-    - https://catboost.ai/en/docs/concepts/python-usages-examples
-    """
-
-    ruleset_params = {"max_rules": max_rules}
-    interpreter = lw.RIPPER(**ruleset_params)
-
+def train_catboost_classifier(train, test, target, max_rules=3):
     # Fit a 'complex' model - CatBoostClassifier
-    X_train = train.drop(['knocked_out_case'], axis=1)
-    y_train = train['knocked_out_case']
-    X_test = test.drop(['knocked_out_case'], axis=1)
-    y_test = test['knocked_out_case']
+    X_train = train.drop([target], axis=1)
+    y_train = train[target]
+    X_test = test.drop([target], axis=1)
+    y_test = test[target]
     categorical_features_indexes = [X_train.columns.get_loc(col) for col in X_train.columns if
                                     X_train[col].dtype == "object"]
     test_pool = Pool(X_test, y_test, cat_features=categorical_features_indexes, feature_names=list(X_test.columns))
 
-    model = CatBoostClassifier(iterations=10, depth=16, loss_function='Logloss', eval_metric="AUC")
+    model = CatBoostClassifier(eval_metric="AUC", learning_rate=0.1, )
     model.fit(X_train, y_train, cat_features=categorical_features_indexes,
               use_best_model=True, eval_set=test_pool)
 
     # Get Catboost model metrics
-    catboost_auc_score = get_catboost_roc_curve(model, X_test, y_test, categorical_features_indexes)
+    # TODO: compute accuracy
 
-    # interpret with wittgenstein and get a ruleset
-    def predict_fn(data, _):
-        res = model.predict(data, prediction_type='Class')
-        res = [x == 'True' for x in res]
-        return res
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(X_train)
+    shap.summary_plot(shap_values, X_train)
 
-    interpret_model(model=model, X=X_train, interpreter=interpreter, model_predict_function=predict_fn)
-    ruleset_model = interpreter
+    catboost_auc_score = get_catboost_roc_auc(model, X_test, y_test, categorical_features_indexes)
 
-    catboost_feature_importances = list(
-        model.get_feature_importance(prettified=True).itertuples(index=False, name=None))
-
-    ruleset_params['catboost'] = model.get_params()
-    ruleset_params['catboost_total_trees'] = model.tree_count_
-
-    return ruleset_model, ruleset_params, model, catboost_feature_importances, catboost_auc_score
+    return model
