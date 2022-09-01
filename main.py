a1b681a5186cb79b1041aa4b00aed2bae6b7a5cb
linomp <lm2p@hotmail.es>
2022-09-01 11:11:34 +0200

    auc~=0.79

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from sklearn.model_selection import train_test_split
 
 if __name__ == '__main__':
     target = 'compliance_2021'
-    dataset = pd.read_csv('data/train.csv')
+    dataset = pd.read_csv('data/inputs/train.csv')
 
     # # split data into train and test
     # train = dataset.sample(frac=0.6)
@@ -14,13 +14,10 @@ if __name__ == '__main__':
     # split stratified with sklearn
     train, test = train_test_split(dataset, test_size=0.33, stratify=dataset[target])
 
-    # replace all nans with 0
-    train.fillna(0, inplace=True)
-
-    # TODO: explore better splitting & data cleaning strategies
+    # TODO: explore better splitting
+    # TODO: data cleaning strategy?  (CATBOOST doesnt need it)
     # TODO: optimize hyperparams, grid search?
     # TODO: add cross validation?
-    # TODO: compute and show accuracy
     # TODO: adapt pipeline to produce output file based on "test" file in expected format
 
     model = train_catboost_classifier(train, test, target=target)
--- utils.py
+++ utils.py
@@ -7,7 +7,7 @@ from catboost.utils import get_roc_curve
 
 
 def get_catboost_roc_auc(model, X, y, categorical_features_indexes):
-    # auc = model.best_score_['validation']['AUC']
+    accuracy = model.score(X, y)
     auc = roc_auc_score(y, model.predict_proba(X)[:, 1])
 
     (fpr, tpr, _) = get_roc_curve(model,
@@ -17,7 +17,7 @@ def get_catboost_roc_auc(model, X, y, categorical_features_indexes):
     ax.plot(fpr, tpr, color="b",
             lw=2,
             alpha=0.8,
-            label="ROC (AUC = %0.2f)" % auc)
+            label=f"ROC (AUC = {auc:.2f} | acc = {accuracy:.3f})")
     ax.plot([0, 1], [0, 1], linestyle="--", lw=2, color="r", label="Random classifier", alpha=0.8)
 
     ax.set(
@@ -37,16 +37,24 @@ def train_catboost_classifier(train, test, target, max_rules=3):
     y_train = train[target]
     X_test = test.drop([target], axis=1)
     y_test = test[target]
+
     categorical_features_indexes = [X_train.columns.get_loc(col) for col in X_train.columns if
                                     X_train[col].dtype == "object"]
     test_pool = Pool(X_test, y_test, cat_features=categorical_features_indexes, feature_names=list(X_test.columns))
 
-    model = CatBoostClassifier(eval_metric="AUC", learning_rate=0.1, )
-    model.fit(X_train, y_train, cat_features=categorical_features_indexes,
-              use_best_model=True, eval_set=test_pool)
+    model = CatBoostClassifier(nan_mode='Min',
+                               thread_count=8,
+                               task_type='CPU',
+                               learning_rate=0.01,
+                               eval_metric="AUC")
+
+    model.fit(X_train,
+              y_train,
+              cat_features=categorical_features_indexes,
+              use_best_model=True,
+              eval_set=test_pool)
 
     # Get Catboost model metrics
-    # TODO: compute accuracy
 
     explainer = shap.TreeExplainer(model)
     shap_values = explainer.shap_values(X_train)
